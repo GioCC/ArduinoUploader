78b29281c02460a2f0c07609efa5e56907cde376
GioCC <25667790+GioCC@users.noreply.github.com>
2019-08-24 14:22:53 +0200

    Add files via upload
    Corrections to file selection process

---
+++ ArduinoPaths.h
@@ -0,0 +1,9 @@
+#pragma once
+const char* ARD_BASE     = "arduino";
+const char* ARD_CORE     = "arduino/hardware/arduino/avr/cores/arduino";
+const char* ARD_CORELIBS = "arduino/hardware/arduino/avr/libraries";
+const char* ARD_LIBS     = "arduino/libraries";
+const char* ARD_LIBS_SRC = "src";
+const char* ARD_LIBS_UTI = "utility";
+const char* ARD_VARIANTS = "arduino/hardware/arduino/avr/variants";
+const char* ARD_TOOLS    = "arduino/hardware/tools/avr/avr";
